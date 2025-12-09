547890bbdb24430b94994d6efd3177fc87f7a8f6
Arjit Rajvanshi <arjitsystem03@gmail.com>
2025-12-10 03:10:15 +0530

    now, only allowed in dev environment

--- reset.go
+++ reset.go
@@ -1,9 +1,27 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	if cfg.platform != "dev" {
+		log.Printf("Platform: %s", cfg.platform)
+		w.WriteHeader(http.StatusForbidden)
+		w.Write(fmt.Append(nil, "Reset is only allowed in dev environment."))
+		return
+	}
+
 	cfg.fileserverHits.Store(0)
+	err := cfg.db.Reset(r.Context())
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(fmt.Appendf(nil, "Failed to reset the database: %s\n", err))
+		return
+	}
+
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hits reset to 0"))
+	w.Write(fmt.Append(nil, "Hits reset to 0 and database reset to initial state."))
 }
